3e7576f5286948ce7766917ddbf03ac566367ec4
Egil Moeller <egil.moller@freecode.no>
2014-01-29 23:44:44 +0100

    Basic geoindex functions based on geohashes

--- cluster.py
+++ cluster.py
@@ -3,6 +3,8 @@ import numpy
 import sys
 import math
 import datetime
+import geohash
+import bitstring
 
 # longitude, latitude
 world = ((-180.0, -90.0), (180.0, 90.0))
@@ -21,6 +23,16 @@ def load_data(filename = "viirs_full_data.csv"):
             points[idx][1] = float(row['latitude'])
 
     return points
+
+
+def spacepartition(min, max):
+    size = (max[0] - min[0], max[1] - min[1])
+    return (
+        ((min[0],               min[1]),               (min[0] + size[0] / 2, min[1] + size[1] / 2)), # NW
+        ((min[0] + size[0] / 2, min[1]),               (max[0],               min[1] + size[1] / 2)), # NE
+        ((min[0] + size[0] / 2, min[1] + size[1] / 2), (max[0],               max[1])),     # SE
+        ((min[0],               min[1] + size[1] / 2), (min[0] + size[0] / 2, max[1]))      # SW
+        )
 
 def create_quadtree(points):
     start_time = datetime.datetime.now()
@@ -54,27 +66,17 @@ def create_quadtree(points):
                 return True
         return False
 
-    def spacepartition(min, max):
-        size = (max[0] - min[0], max[1] - min[1])
-        return (
-            ((min[0],               min[1]),               (min[0] + size[0] / 2, min[1] + size[1] / 2)), # NW
-            ((min[0] + size[0] / 2, min[1]),               (max[0],               min[1] + size[1] / 2)), # NE
-            ((min[0] + size[0] / 2, min[1] + size[1] / 2), (max[0],               max[1])),     # SE
-            ((min[0],               min[1] + size[1] / 2), (min[0] + size[0] / 2, max[1]))      # SW
-            )
-
     # Format: (start, end, child1, child2, child3, child4)
     # where the start/end are indexes into points and childx:es are an indexes into quadtree
     quadtree = numpy.ones((points.shape[0] * points.itemsize * 8 * 2, 6)) # *8 since 8 bits to a byte, *2 to account for 4 way branching...
     quadtree *= -1 # Just make sure we have "null pointers" everywhere to start with...
     quadtreepos = [0]
 
-    def partition(start, end, min, max, prefix = ""):
-        # print prefix + "PART", start, end, min, max
+    def partition(start, end, min, max):
         res = quadtreepos[0]
         quadtreepos[0] += 1
         quadtree[res] = (start, end, -1, -1, -1, -1)
-        if end - start > 1:
+        if end - start > 1 and check_different(start, end):
             partitions = spacepartition(min, max)
 
             pstart = start
@@ -83,16 +85,8 @@ def create_quadtree(points):
                     pend = split(pstart, end, *partitions[pidx])
                 else:
                     pend = end
-                different = check_different(pstart, pend)
-                # if not check_inside(pstart, pend, *partitions[pidx]):
-                #     print prefix + "  NOT INSIDE", pstart, pend, partitions[pidx]
-                #     raise Exception
-                if pend - pstart and different:
-                    quadtree[res][2 + pidx] = partition(pstart, pend, prefix=prefix+" ", *partitions[pidx])
-                # if pend == pstart:
-                #     print prefix + "  NULL ", pstart, pend, partitions[pidx]
-                # elif not different:
-                #     print prefix + "  SAME ", pstart, pend, partitions[pidx]
+                if pend - pstart:
+                    quadtree[res][2 + pidx] = partition(pstart, pend, *partitions[pidx])
                 pstart = pend
         return res
     partition(0, points.shape[0], *world)
@@ -101,4 +95,112 @@ def create_quadtree(points):
     print "Exec time:", end_time - start_time
 
     return quadtree
-    
+
+def print_quadtree(data, tree, maxlevel=5, pos=0, prefix='', min=world[0], max=world[1]):
+    if tree[pos][2] != -1 or tree[pos][3] != -1 or tree[pos][4] != -1 or tree[pos][5] != -1:
+        print prefix + "[%s,%s-%s,%s]: %s" % (min[0], min[1], max[0], max[1], int(tree[pos][1] - tree[pos][0]))
+        partitions = spacepartition(min, max)
+        for pidx in xrange(0, 4):
+            if tree[pos][2 + pidx] != -1:
+                print_quadtree(data, tree, maxlevel, tree[pos][2 + pidx], prefix + '  ', *partitions[pidx])
+    else:
+        for pidx in xrange(int(tree[pos][0]), int(tree[pos][1])):
+            point = data[pidx]
+            print prefix + "%s,%s" % (point[0], point[1])
+
+
+def load_hashes(filename = "viirs_full_data.csv", precision = 12):
+    with open(filename) as f:
+        res = [
+            geohash.encode(float(row['latitude']), float(row['longitude']), precision)
+            for idx, row in enumerate(csv.DictReader(f))]
+        res.sort()
+        return res
+
+
+def geohash_to_binary(hashcode):
+    res = bitstring.BitArray()
+    pos = 0
+    for i in hashcode:
+        res.append(bitstring.BitArray(uint=geohash._base32_map[i], length=5))
+    return res
+
+def binary_to_geohash(bincode):
+    res = []
+    for i in bincode.cut(5):
+        res.append(geohash._base32[i.uint])
+    return ''.join(res)
+
+def binary_inc(str):
+    str.reverse()
+    for pos in xrange(0, len(str) + 1):
+        if pos == len(str):
+            str += '0b1'
+            break
+        if not str[pos]:
+            str[pos] = 1
+            break
+        str[pos] = 0
+    str.reverse()
+    return str
+
+def binary_search(key, lst):
+    """Binary search a sorted list, returning the first item equal to or greater than key."""
+    size = len(lst)
+    step = size / 2 or 1
+    pos = size / 2 or 1
+
+    while True:
+        step = (step / 2) or 1
+
+        if step <= 0:
+            raise Exception("Step <= 0")
+        elif pos > size:
+            raise Exception("pos > size")
+        elif pos < 0:
+            raise Exception("pos < 0")
+        assert step > 0 and pos <= size and pos >= 0
+
+        if pos == size:
+            if lst[pos - 1] < key:
+                return pos
+            else:
+                pos -= step
+        elif lst[pos] < key:
+            pos += step
+        elif lst[pos] >= key:
+            if pos < 1 or lst[pos - 1] < key:
+                return pos
+            else:
+                pos -= step
+        else:
+            return pos
+
+def find_range(prefix, lst):
+    """Binary search a sorted list of geohashes
+    Returns the range of indexes [lower, upper] that share the prefix
+    """
+
+    lower = binary_search(prefix, lst)
+    binprefix = geohash_to_binary(prefix)
+    if binprefix.all(1):
+        upper = len(lst)
+    else:
+        upper = binary_search(
+            binary_to_geohash(
+                binary_inc(binprefix)),
+            lst)
+
+    return (lower, upper)
+
+
+
+# geohash.bbox('0m1g52jnur27')
+# {'s': -61.34645814076066, 'e': -166.1391907185316, 'w': -166.13919105380774, 'n': -61.3464579731226}
+
+def get_nrpoints(prefix):
+    pass
+
+def build_locality(q, S):
+    pass
+
